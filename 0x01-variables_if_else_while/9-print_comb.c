9089bd4e5dc9bb46bc7bd615a779a40bd7a3fe4a
ogemboeugene <7103.2021@students.ku.ac.ke>
2023-09-08 12:21:40 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point main
+ * Description - prints all single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(48 + x);
+		if (x != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
+
